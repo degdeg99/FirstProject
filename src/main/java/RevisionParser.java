3af32518b6f6a161b9287b05f0998f4f21e60b15
Dylan Rankin <drankin@bsu.edu>
2020-09-28 18:57:51 -0400

    Made the RevisionParser class and created the EditorParser class changed the gradle to allow us to use Json Some File clean up -Dylan Rankin

--- src/main/java/RevisionParser.java
+++ src/main/java/RevisionParser.java
@@ -1,2 +1,27 @@
-public class RevisionParser {
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Map;
+
+public class RevisionParser {// this is to parse the revisions out of the JSON file
+
+    public JsonArray GetRevisions(InputStream connection){
+    Reader reader = new InputStreamReader(connection);
+    JsonElement rootElement = JsonParser.parseReader(reader);
+    JsonObject rootObject = rootElement.getAsJsonObject();
+    JsonObject pages = rootObject.getAsJsonObject("query").getAsJsonObject("pages");
+    JsonArray revisions = null;
+
+
+    for(Map.Entry<String, JsonElement> entry :  pages.entrySet()){
+        JsonObject entryObject = (JsonObject) entry.getValue();
+        revisions = entryObject.getAsJsonArray("revisions");
+    }
+    return revisions;
+    }
 }
