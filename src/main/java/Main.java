d08403057104c9d724843a9ac1db2205fa122f95
JpSymanski <70335886+jpsymanski@users.noreply.github.com>
2020-10-05 20:57:53 -0500

    Created simple Gui

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,10 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,6 +15,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        primaryStage.setTitle("Wikipedia editor search");
+        Button SearchButton = new Button("Search");
+        TextField SearchTF = new TextField("");
+        GridPane r = new GridPane();
+        r.addRow(4,SearchButton,SearchTF);
+        Scene scene = new Scene(r, 400, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
 
     }
 }
